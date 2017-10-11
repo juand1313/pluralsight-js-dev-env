799b7d4e5d54279ee3c4774afa0a7d44833d5b8a
juand1313 <juandiego-1392@hotmail.com>
2017-10-11 09:20:33 -0600

    Course ended

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://lit-hamlet-92373.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
